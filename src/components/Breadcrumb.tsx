36a25bc33f632f3d86c6b814dc7cc6e6ca75caf5
Aman Gupta <amang4885@gmail.com>
2024-12-05 00:21:38 +0530

    some changes in breadcrumb in upadet page and detail page

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -6,11 +6,16 @@ function handleClick(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
   event.preventDefault();
 }
 
-const Breadcrumb = () => {
+const Breadcrumb = ({ projectName }: { projectName?: string }) => {
   const location = useLocation();
   const navigate = useNavigate();
-  const pathSegments = location.pathname.split('/').filter((segment) => segment && segment !== 'edit'); ;
 
+  const pathSegments = location.pathname.split('/').filter((segment) => segment && segment !== 'edit'); 
+  if(parseInt(pathSegments[pathSegments.length-1])){
+    pathSegments.pop()
+    pathSegments.push(projectName || "");
+  }
+  
   const buildPath = (index: number) =>
     `/${pathSegments.slice(0, index + 1).join('/')}`;
   return (
