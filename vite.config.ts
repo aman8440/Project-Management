8769ecf60ec99a245f239a786c186f24c294eacb
Aman Gupta <amang4885@gmail.com>
2024-12-25 15:51:36 +0530

    changes in vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/truck_management': {
-        target: 'http://localhost',
-        changeOrigin: true
-      }
-    }
-  }
+  plugins: [react()]
 })
