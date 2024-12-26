327fbdc436f6a07f71f56d7e2fd4d9da652d94dc
Aman Gupta <amang4885@gmail.com>
2024-12-26 11:33:25 +0530

    changes in export functionality on listing page and change color of loader

--- src/pages/private-layout/Dashboard.tsx
+++ src/pages/private-layout/Dashboard.tsx
@@ -1,12 +1,8 @@
 import './dashboard.css'
-import Breadcrumb from "../../components/Breadcrumb"
 
 const Dashboard = () => {
   return (
     <div className="main-container d-flex flex-column">
-      <div className="sub-container d-flex justify-content-start">
-        <Breadcrumb/>
-      </div>
       <div className="primary-container d-flex justify-content-center align-items-center">
         <h1 className="display-4 font-weight-bold">Coming Soon</h1>
       </div>
